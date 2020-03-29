403916b82de6c3de23726223af44764dc41fb110
Piotr Grajeta <piotr@fedora.localdomain>
2020-03-29 15:36:00 +0200

    added preapending to the path

--- myls.pl
+++ myls.pl
@@ -1,10 +1,13 @@
 #!/usr/bin/perl
 
 use strict;
+use File::stat;
+use Stat::lsMode;
+use POSIX qw(strftime);
 
-my $dir = "./";
-my $LFlag = 0;
-my $lFlag = 0;
+my $dir = "./"; #deafult dir
+my $LFlag = 0; #name of the owner of the file
+my $lFlag = 0; #name of the file, size, modify date, permissons
 
 foreach my $var(@ARGV){
     if (($var =~ /-.n*([^lL])/)) {
@@ -17,9 +20,23 @@ foreach my $var(@ARGV){
     if(($var =~ /-.*[L]/)){
 	$LFlag = 1;
     }
-    if(($var =~ /^[^-].+/)){
+    if(($var =~ /^[^-].*/)){
 	$dir = $var;
     }
 }
-print "Lflag:$LFlag\nlFlag:$lFlag\ndir:$dir\n";
 
+print "Lflag:$LFlag\nlFlag:$lFlag\ndir:$dir\n\n";
+
+opendir(my $dh, $dir) || die "Can't opendir $dir: $!";
+my @dots = grep {/^[^.].*/ && "$dir/$_"} readdir($dh);
+foreach my $thing (sort @dots){
+    my $sb = stat("$dir/$thing");
+    my $time = strftime  "%Y-%m-%d %H:%M:%S", localtime $sb->mtime;
+    my $size = $sb->size;
+    my $mode = $sb->mode;
+    my $lsmode = file_mode("$dir/$thing");
+    my $ownername = getpwuid((stat("$dir/$thing"))[4]);
+
+    print "$thing\t$size\t$time\t$lsmode\t$ownername\n";
+}
+closedir $dh;
